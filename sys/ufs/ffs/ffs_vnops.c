0289dec92b9b0b48d938d86e709c996fefb47638
Thomas Munro <tmunro@FreeBSD.org>
2021-01-07 23:50:59 +1300

    ffs: Suport O_DSYNC.
    Respect the new IO_DATASYNC flag when performing synchronous writes.
    Compared to O_SYNC, O_DSYNC lets us skip updating the inode in some
    cases, matching the behaviour of fdatasync(2).
    
    Reviewed by: kib
    Differential Review: https://reviews.freebsd.org/D25160

--- sys/ufs/ffs/ffs_vnops.c
+++ sys/ufs/ffs/ffs_vnops.c
@@ -1001,9 +1001,12 @@ ffs_write(ap)
 			uio->uio_resid = resid;
 		}
 	} else if (resid > uio->uio_resid && (ioflag & IO_SYNC)) {
-		error = ffs_update(vp, 1);
-		if (ffs_fsfail_cleanup(VFSTOUFS(vp->v_mount), error))
-			error = ENXIO;
+		if (!(ioflag & IO_DATASYNC) ||
+		    (ip->i_flags & (IN_SIZEMOD | IN_IBLKDATA))) {
+			error = ffs_update(vp, 1);
+			if (ffs_fsfail_cleanup(VFSTOUFS(vp->v_mount), error))
+				error = ENXIO;
+		}
 	}
 	return (error);
 }
